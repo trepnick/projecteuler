5242746f64f241233e0289a5fb87342fe0f3e6e0
Ben Haines <bfhain@gmail.com>
2020-03-20 23:01:52 -0400

    Latest

--- problem14.py
+++ problem14.py
@@ -1,36 +1,34 @@
 
-
-def collatzlength(n):
-    obj = Collatz(n)
-    ret = 1
-    for i in obj:
-        try:
-            ret += Collatz.lengths[i]
-            break
-        except KeyError:
-            ret += 1
-    Collatz.lengths[n] = ret
-    return ret
-
-
 class Collatz:
     lengths = {1: 1}
 
     def __init__(self, initial_value):
-        self.initial_value = initial_value
-        self.current_value = initial_value
+        self._initial_value = initial_value
+        self._current_value = initial_value
+        visits = [self._initial_value]
+        length = 1
+        for i in self:
+            try:
+                length += Collatz.lengths[i]
+                break
+            except KeyError:
+                length += 1
+                visits.append(i)
+
+        for idx, val in enumerate(visits):
+            Collatz.lengths[val] = length - idx
 
     def __next__(self):
-        if self.current_value % 2 == 0:
-            self.current_value /= 2
+        if self._current_value % 2 == 0:
+            self._current_value = self._current_value // 2
         else:
-            self.current_value = self.current_value * 3 + 1
-        return self.current_value
+            self._current_value = (self._current_value * 3 + 1)
+        return self._current_value
 
     def __iter__(self):
         return self
 
 
 if __name__ == '__main__':
-    print(collatzlength(10))
+    Collatz(10)
     pass
